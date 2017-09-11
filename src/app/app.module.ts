dd571afaf63f94dccdf8b54e7168924b47e95ae9
nvazquezg <nvazquezg@gmail.com>
2017-09-11 14:54:10 +0200

    Includes a basic NavBarComponent to show top header with actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import {MdButtonModule, MdCheckboxModule, MdIconModule, MdToolbarModule} from '@angular/material';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
+    MdToolbarModule,
+    MdIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
