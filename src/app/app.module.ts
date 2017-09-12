b231f929e87eb7676c12857067fddbb710da57fd
nvazquezg <nvazquezg@gmail.com>
2017-09-12 12:15:19 +0200

    Shows forms to compare same and distinct species
    The forms are similar to the old version of EvoPPI and are loaded with mock data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MdButtonModule, MdCheckboxModule, MdIconModule, MdTabsModule, MdToolbarModule} from '@angular/material';
+import {
+  MdButtonModule, MdCheckboxModule, MdIconModule, MdInputModule, MdSelectModule, MdTableModule, MdTabsModule,
+  MdToolbarModule
+} from '@angular/material';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormSameSpeciesComponent } from './form-same-species/form-same-species.component';
 import { FormDistinctSpeciesComponent } from './form-distinct-species/form-distinct-species.component';
+import {CdkTableModule} from "@angular/cdk/table";
 
 @NgModule({
   imports: [
@@ -14,14 +18,22 @@ import { FormDistinctSpeciesComponent } from './form-distinct-species/form-disti
     MdCheckboxModule,
     MdToolbarModule,
     MdIconModule,
-    MdTabsModule
+    MdTabsModule,
+    MdInputModule,
+    MdSelectModule,
+    MdTableModule,
+    CdkTableModule,
   ],
   exports: [
     MdButtonModule,
     MdCheckboxModule,
     MdToolbarModule,
     MdIconModule,
-    MdTabsModule
+    MdTabsModule,
+    MdInputModule,
+    MdSelectModule,
+    MdTableModule,
+    CdkTableModule,
   ],
 })
 export class MaterialModule { }
--- src/app/form-same-species/form-same-species.component.ts
+++ src/app/form-same-species/form-same-species.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DataSource} from "@angular/cdk/collections";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-form-same-species',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-same-species.component.css']
 })
 export class FormSameSpeciesComponent implements OnInit {
+  dataSource = new SameSpeciesDataSource();
+  displayedColumns = ['Gene', 'Interacts', 'Code'];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export interface ElementSameSpecies {
+  gene: string;
+  interacts: string;
+  code: string;
+}
+
+const data: ElementSameSpecies[] = [
+  {gene: "gene1", interacts:"interacts1", code:"code1"},
+  {gene: "gene2", interacts:"interacts2", code:"code2"}
+];
+
+
+export class SameSpeciesDataSource extends DataSource<any> {
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<ElementSameSpecies[]> {
+    return Observable.of(data);
+  }
+
+  disconnect() {}
 }
