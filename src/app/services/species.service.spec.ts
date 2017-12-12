caf21e3283243a7437755461416cfcdf29092d3a
Noé Vázquez González <nvazquezg@gmail.com>
2017-12-12 21:55:57 +0100

    Adds some basic tests for SpeciesService

--- src/app/services/species.service.spec.ts
+++ src/app/services/species.service.spec.ts
@@ -1,7 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { SpeciesService } from './species.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Species} from '../interfaces/species';
+import {Observable} from 'rxjs/Observable';
+
+export const SPECIES: Species[] = [
+  {id: 1, name: 'Homo Sapiens', interactomes: []}
+];
 
 describe('SpeciesService', () => {
   beforeEach(() => {
@@ -13,5 +19,25 @@ describe('SpeciesService', () => {
 
   it('should be created', inject([SpeciesService], (service: SpeciesService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should be instance of SpeciesService', inject([SpeciesService], (service: SpeciesService) => {
+    expect(service instanceof SpeciesService).toBeTruthy();
+  }));
+
+  it('can instantiate service with "new"', inject([HttpClient], (http: HttpClient) => {
+    expect(http).not.toBeNull('http should be provided');
+    const service = new SpeciesService(http);
+    expect(service instanceof SpeciesService).toBe(true, 'new service should be ok');
+  }));
+
+  it('can call getSpecies', inject([SpeciesService], (service: SpeciesService) => {
+    spyOn(service, 'getSpecies').and.returnValue(Observable.of(SPECIES));
+    service.getSpecies().subscribe((species) => {
+      expect(species instanceof Array).toBeTruthy();
+      expect(species.length).toBe(1);
+      expect(species[0].name).toBe('Homo Sapiens');
+    });
+    expect(service.getSpecies).toHaveBeenCalled();
   }));
 });
