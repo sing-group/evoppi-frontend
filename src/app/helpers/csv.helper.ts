b0e67f504e4c8c5de4bd49d82a2870f3836e3ecb
Noé Vázquez González <nvazquezg@gmail.com>
2018-01-24 21:16:56 +0100

    Improves CSV compatibility

--- src/app/helpers/csv.helper.ts
+++ src/app/helpers/csv.helper.ts
@@ -1,9 +1,9 @@
 
 export class CsvHelper {
   public static getCSV(headers: Array<string>, data: Array<Array<string>>): string {
-    let csv = 'data:text/csv;charset=utf-8,' + headers.join(',') + '\n';
+    let csv = 'data:text/csv;charset=utf-8,"' + headers.join('";"') + '"\n';
     for (const row of data){
-      csv += row.join(',') + '\n';
+      csv += '"' + row.join('";"') + '"\n';
     }
     return encodeURI(csv);
   }
