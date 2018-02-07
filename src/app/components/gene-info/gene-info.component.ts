63a84d5e882e43374bb9d8afb264f57a7178ae65
Noé Vázquez González <nvazquezg@gmail.com>
2018-02-07 21:19:01 +0100

    Enables download of sequences in GeneInfo component

--- src/app/components/gene-info/gene-info.component.ts
+++ src/app/components/gene-info/gene-info.component.ts
@@ -3,6 +3,9 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 import {Gene} from '../../interfaces/gene';
 import {BlastResult} from '../../interfaces/blast-result';
 import {GeneService} from '../../services/gene.service';
+import {SafeResourceUrl} from '@angular/platform-browser/src/security/dom_sanitization_service';
+import {DomSanitizer} from '@angular/platform-browser';
+import {FastaHelper} from '../../helpers/fasta.helper';
 
 @Component({
   selector: 'app-gene-info',
@@ -13,14 +16,18 @@ export class GeneInfoComponent implements OnInit {
 
   public gene: Gene;
   public blastResults: BlastResult[];
+  public sequences: SafeResourceUrl = '';
 
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private geneService: GeneService) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private geneService: GeneService, private domSanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.geneService.getGene(this.data.geneId).subscribe((res) => {
       this.gene = res;
       this.blastResults = this.data.blastResults;
+      this.sequences = this.domSanitizer.bypassSecurityTrustResourceUrl(
+        FastaHelper.getFasta(this.gene.id.toString(), this.gene.sequences)
+      );
     });
   }
 
---
+++ src/app/helpers/fasta.helper.ts
@@ -0,0 +1,20 @@
+
+export class FastaHelper {
+  public static getFasta(header: string, data: Array<string>): string {
+    let res = 'data:text/x-fasta;base64,';
+    let plain = '';
+    for (const row of data){
+      plain += '>' + header + '\n';
+      let aux: string;
+      let i = 0;
+      do {
+         aux = row.substring(i * 80, i * 80 + 80);
+         plain += aux + '\n';
+         ++i;
+      } while (aux.length === 80);
+      plain += '\n';
+    }
+    res += btoa(plain);
+    return encodeURI(res);
+  }
+}
